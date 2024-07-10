1ae6078b837bee14c50a49434a7c98b9051a5c6c
petersudai <psudai@gmail.com>
2024-07-10 11:41:10 +0000

    Minimum Operations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,1 +1,26 @@
+#!/usr/bin/python3
+"""
+a method that calculates the fewest number of operations
+needed to result in exactly n H characters in the file.
+"""
 
+
+def minOperations(n):
+    """
+    Calculate the fewest number of operations 
+    needed to result in exactly n 'H' characters
+    """
+
+    if n <= 1:
+        return 0
+
+    operations = 0
+    factor = 2
+
+    while n > 1:
+        while n % factor == 0:
+            operations += factor
+            n //= factor
+        factor += 1
+
+    return operations
