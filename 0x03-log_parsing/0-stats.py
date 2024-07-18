c28f3ea82799f82ee271a7f55c8f3349a9978e24
petersudai <psudai@gmail.com>
2024-07-18 15:48:19 +0000

    Log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 import sys
+import signal
 
 
 total_size = 0
 status_counts = {200: 0, 301: 0, 400: 0, 403: 0, 404: 0, 405: 0, 500: 0}
 line_count = 0
+
 
 def print_stats(total_size, status_counts):
     """
@@ -15,29 +17,42 @@ def print_stats(total_size, status_counts):
         if status_counts[status] > 0:
             print(f"{status}: {status_counts[status]}")
 
+
+def signal_handler(sig, frM):
+    """
+    Handle keyboard interruption signal
+    """
+    print_stats(total_size, status_counts)
+    sys.exit(0)
+
+signal.signal(signal.SIGINT, signal_handler)
+
 try:
     for line in sys.stdin:
         parts = line.split()
         
-        if len(parts) > 6:
-            try:
-                status_code = int(parts[-2])
-                file_size = int(parts[-1])
+        if len(parts) < 7:
+            continue
+        
+        try:
+            status_code = int(parts[-2])
+            file_size = int(parts[-1])
+            
+            total_size += file_size
 
-                total_size += file_size
+            if status_code in status_counts:
+                status_counts[status_code] += 1
 
-                if status_code in status_counts:
-                    status_counts[status_code] += 1
+            line_count += 1
 
-                line_count += 1
+            if line_count % 10 == 0:
+                print_stats(total_size, status_counts)
 
-                if line_count % 10 == 0:
-                    print_stats(total_size, status_counts)
-
-            except (ValueError, IndexError):
-                continue
+        except (ValueError, IndexError):
+            continue
 
 except KeyboardInterrupt:
-    pass
-finally:
     print_stats(total_size, status_counts)
+    sys.exit(0)
+
+print_stats(total_size, status_counts)
