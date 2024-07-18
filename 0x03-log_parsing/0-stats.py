a72a91b47865ab67096d5bc05cc761b8638a36ea
petersudai <psudai@gmail.com>
2024-07-18 18:31:45 +0000

    Log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     count = 0
     codes = ["200", "301", "400", "401", "403", "404", "405", "500"]
     stats = {k: 0 for k in codes}
-    
+
     def print_stats(stats: dict, file_size: int) -> None:
         """
         Print the accumulated statistics.
