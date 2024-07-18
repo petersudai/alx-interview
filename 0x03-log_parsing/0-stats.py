0bf84717a6cb7acc0a3b1bd7bb98adaed5193057
petersudai <psudai@gmail.com>
2024-07-18 18:30:32 +0000

    Log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,23 +1,24 @@
 #!/usr/bin/python3
-"""log parsing"""
+"""
+Log parsing
+"""
 
 import sys
-
 
 if __name__ == "__main__":
     total_size = 0
     count = 0
     codes = ["200", "301", "400", "401", "403", "404", "405", "500"]
     stats = {k: 0 for k in codes}
-
+    
     def print_stats(stats: dict, file_size: int) -> None:
-    """
-    Print the accumulated statistics.
-    """
-    print("File size: {:d}".format(file_size))
-    for k in sorted(stats.keys()):
-        if stats[k]:
-            print("{}: {}".format(k, stats[k]))
+        """
+        Print the accumulated statistics.
+        """
+        print("File size: {:d}".format(file_size))
+        for k in sorted(stats.keys()):
+            if stats[k]:
+                print("{}: {}".format(k, stats[k]))
 
     try:
         for line in sys.stdin:
