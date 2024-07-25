cb7e0ab855ec50ede5d82f5d0f75a301b21eafd0
petersudai <psudai@gmail.com>
2024-07-25 10:46:50 +0000

    UTF-8 Validation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -3,29 +3,28 @@
 method that determines if a given data set represents a valid UTF-8 encoding
 """
 def validUTF8(data):
-    def get_num_of_bytes(num):
-        if num & 0x80 == 0x00:
-            return 1
-        elif num & 0xE0 == 0xC0:
-            return 2
-        elif num & 0xF0 == 0xE0:
-            return 3
-        elif num & 0xF8 == 0xF0:
-            return 4
-        return -1
-
     num_bytes = 0
 
+    mask1 = 1 << 7
+    mask2 = 1 << 6
+
     for num in data:
+        mask = 1 << 7
         if num_bytes == 0:
-            num_bytes = get_num_of_bytes(num)
-            if num_bytes == -1:
+            while mask & num:
+                num_bytes += 1
+                mask = mask >> 1
+
+            if num_bytes == 0:
+                continue
+
+            if num_bytes == 1 or num_bytes > 4:
                 return False
-            if num_bytes > 1:
-                num_bytes -= 1
+
         else:
-            if num & 0xC0 != 0x80:
+            if not (num & mask1 and not (num & mask2)):
                 return False
-            num_bytes -= 1
+
+        num_bytes -= 1
 
     return num_bytes == 0
