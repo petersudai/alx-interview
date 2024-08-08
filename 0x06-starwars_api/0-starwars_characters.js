1ebd70a06076358d5e8d9cd93cb1aff94f7437b4
petersudai <psudai@gmail.com>
2024-08-08 17:48:36 +0000

    Star Wars Characters

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,28 +1,52 @@
-#!/usr/bin/node
+#!/usr/bin/env node
 
-const request = require('request');
+const util = require('util');
+const request = util.promisify(require('request'));
 
-const movieId = process.argv[2];
-const url = `https://swapi-api.hbtn.io/api/films/${movieId}/`;
+const filmID = process.argv[2];
 
-request(url, (error, response, body) => {
-  if (error) {
-    console.error(error);
-    return;
+async function fetchCharacter(url) {
+  try {
+    let character = await (await request(url)).body;
+    return JSON.parse(character).name;
+  } catch (error) {
+    console.error(`Failed to fetch character at ${url}:`, error);
+    return null;
   }
+}
 
-  const data = JSON.parse(body);
-  const characters = data.characters;
+async function fetchFilmDetails(filmId) {
+  const endpoint = `https://swapi-api.hbtn.io/api/films/${filmId}`;
+  try {
+    let response = await (await request(endpoint)).body;
+    return JSON.parse(response);
+  } catch (error) {
+    console.error(`Failed to fetch film details for film ID ${filmId}:`, error);
+    process.exit(1);
+  }
+}
 
-  characters.forEach((character) => {
-    request(character, (error, response, body) => {
-      if (error) {
-        console.error(error);
-        return;
-      }
+async function displayCharacters(characterUrls) {
+  console.log('Fetching character names, please wait...\n');
+  const promises = characterUrls.map(fetchCharacter);
+  const characterNames = await Promise.all(promises);
 
-      const characterData = JSON.parse(body);
-      console.log(characterData.name);
-    });
+  characterNames.forEach((name, index) => {
+    if (name) {
+      console.log(`${index + 1}. ${name}`);
+    }
   });
-});
+}
+
+async function starwarsCharacters(filmId) {
+  const filmDetails = await fetchFilmDetails(filmId);
+  const characterUrls = filmDetails.characters;
+  await displayCharacters(characterUrls);
+}
+
+if (!filmID) {
+  console.error('Please provide a valid Star Wars movie ID.');
+  process.exit(1);
+}
+
+starwarsCharacters(filmID);
