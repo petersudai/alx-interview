f6bba79f0b57df364073feb857f455877b8548a5
petersudai <81692851+petersudai@users.noreply.github.com>
2024-08-21 17:50:03 +0300

    Change come from within

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+This module provides solution to the "Change comes from within" problem
+"""
+
+def makeChange(coins, total):
+    """
+    Determines the fewest number of coins needed to make the given total
+    """
+    if total <= 0:
+        return 0
+
+    dp = [total + 1] * (total + 1)
+    dp[0] = -
+
+    # Fill dp array using dynamic programming
+    for coin in coins:
+        for i in range(coin, total + 1):
+            dp[i] = min(dp[i], dp[i - coin] + 1)
+
+    return dp[total] if dp[total] != total + 1 else -1
